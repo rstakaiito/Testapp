5edf6914a7b43ea863cc88fd1951020a464d7367
rstakaiito <vietcuong.it@gmail.com>
2013-11-12 10:38:55 +0700

    Test commit

---
+++ Dribbble - profile_activity_large.png by Mason Yarnell_files/deckDB_js.php
@@ -0,0 +1,26 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<A HREF="http://ad.doubleclick.net/jump/N5192.148401.THEDECK/B7325765.4;sz=120x90;pc=[TPAS_ID];ord=1383811204?">';
+advert+='<IMG class="ad" SRC="http://ad.doubleclick.net/ad/N5192.148401.THEDECK/B7325765.4;sz=120x90;pc=[TPAS_ID];ord=1383811204?" BORDER=0 WIDTH=120 HEIGHT=90 ALT="Advertisement"></A>';
+advert+='<p class="ads">';
+advert+='<A HREF="http://ad.doubleclick.net/jump/N5192.148401.THEDECK/B7325765.4;sz=120x90;pc=[TPAS_ID];ord=1383811204?">iStock by Getty Images.</a> Search. Find. Standout.';
+advert+='</p>';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+advert+='';
+
+
+
+
+document.write(advert);
+
+
